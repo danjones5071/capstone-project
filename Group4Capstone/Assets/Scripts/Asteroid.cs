bd102184442e05f902dc7052b26e509dbe14d16b
Roberto <moltrex@gmail.com>
2018-09-02 20:28:23 -0400

    Added soft Y movement to asteroids

--- Group4Capstone/Assets/Scripts/Asteroid.cs
+++ Group4Capstone/Assets/Scripts/Asteroid.cs
@@ -16,8 +16,9 @@ public class Asteroid : MonoBehaviour
 	// Declare variables to store physics and position related values for our obstacle.
 	private float _createPosX;			// Horizontal position of the object.
 	private float _posY;				// Vertical position of the object.
-	private float _speed;				// Speed at which the object moves.
-	private float _spin;				// Angular velocity of the object.
+	private float _speedX;				// Speed at which the object moves.
+    private float _speedY;              // Speed at which the object moves.
+    private float _spin;				// Angular velocity of the object.
 
 	void Awake()
 	{
@@ -31,15 +32,16 @@ public class Asteroid : MonoBehaviour
 	{
 		// Randomize physical attributes of our new asteroid.
 		_posY = Random.Range( -5.0f, 5.0f  );			// Randomize the vertical position of the object.
-		_speed = Random.Range( 3.0f, 5.0f );			// Randomize the speed of the object.
-		_spin = Random.Range( -50.0f, 50.0f );			// Randomize the angular velocity of the object.
+		_speedX = Random.Range( 3.0f, 5.0f );			// Randomize the speed of the object.
+        _speedY = Random.Range(-1.0f, 1.0f);         // Randomize the speed of the object.
+        _spin = Random.Range( -50.0f, 50.0f );			// Randomize the angular velocity of the object.
 
 		// Apply move the object to the desired position.
 		objTransform.position = new Vector2( _createPosX, _posY );
 
-		// Apply physical values to the object's rigidbody.
-		objRigid.velocity = Vector2.left * _speed;	// Multiply the left vector by our speed to obtain velocity.
-		objRigid.angularVelocity = _spin;			// Apply angular velocity to create a spin.
+        // Apply physical values to the object's rigidbody.
+        objRigid.velocity = new Vector2((Vector2.left * _speedX).x, _posY * _speedY);// Multiply the left vector by our speed to obtain velocity.
+        objRigid.angularVelocity = _spin;			// Apply angular velocity to create a spin.
 	}
 
 	void OnCollisionEnter2D( Collision2D col )
