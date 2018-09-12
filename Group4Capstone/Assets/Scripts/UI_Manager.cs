87a97f7dd1446aa15e81c7c3b501a894cfd4b9a1
Roberto <moltrex@gmail.com>
2018-09-12 19:29:00 -0400

    Added the Starting Point of the User Interface

---
+++ Group4Capstone/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+
+public class UI_Manager : MonoBehaviour {
+   
+    private float startTime;
+    private float secondsElapsed;
+
+    [SerializeField]
+    private Text gameTimeElapsedRef;
+   
+    // Use this for initialization
+    void Start ()
+    {  
+        startTime = Time.time;      
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        secondsElapsed = Time.time - startTime;
+
+        int minutes = ((int)secondsElapsed / 60);
+        int seconds = ((int)(secondsElapsed % 60));
+
+        string temp = "0" + minutes;
+        string minutesString = (minutes < 10 ? temp : minutes.ToString());
+
+        temp = "0" + seconds;
+        string secondsString = (seconds < 10 ? temp : seconds.ToString());
+
+        gameTimeElapsedRef.text = (minutesString + ":" + secondsString);
+     
+    }
+}
