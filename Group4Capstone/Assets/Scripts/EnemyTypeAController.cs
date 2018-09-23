c5b8b8da96a73af19bfc96ffa9948d56331b5edf
adrian.gonzalez <gonzalez.adrianj@gmail.com>
2018-09-23 01:26:48 -0700

    Forgot to commit EnemyTypeAController script
    Forgot to commit EnemyTypeAController script

--- Group4Capstone/Assets/Scripts/EnemyTypeAController.cs
+++ Group4Capstone/Assets/Scripts/EnemyTypeAController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyTypeAController : MonoBehaviour {
 
-    public GameObject player;
+    //public EnemyAI enemyAIScript;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        // Need to be able to set player location but can't access script
+        // because script is in main Assets folder.
+        //enemyAIScript = GetComponent<EnemyAI>();
+        //enemyAIScript.playerLocation = References.global.playerTrans;
+        //enemyAIScript.enemyArea = GameObject.Find("EnemyArea");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
