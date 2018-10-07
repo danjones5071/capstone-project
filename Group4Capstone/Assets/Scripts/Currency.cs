065ac9632f46286c82d290e6e5e4a34b263b8505
Dan Jones <dan@ogovo.com>
2018-10-07 19:27:11 -0400

    Add Currency Generator.

---
+++ Group4Capstone/Assets/Scripts/Currency.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Currency : MonoBehaviour
+{
+	// Declare variables to cache necessary components.
+	private Rigidbody2D coinRigid;		// Coin's rigidbody component.
+	private Transform coinTransform;	// Coin's transform component.
+	private float speedX;				// Speed at which the object moves.
+	private float createPosX;
+
+	// Use this for initialization
+	void Awake()
+	{
+		coinRigid = GetComponent<Rigidbody2D>();	// Cache a reference to the object's rigidbody component.
+		coinTransform = transform;
+		createPosX = 15.0f;							// Initialize the horizontal position for object generation.
+	}
+	void Start ()
+	{
+		
+		// Randomize physical attributes of our new asteroid.
+		float posY = Random.Range( -4.9f, 4.9f  );		// Randomize the vertical position of the object.
+		speedX = Random.Range( 1.0f, 2.5f );			// Randomize the horizontal speed of the object.
+
+		// Apply move the object to the desired position.
+		coinTransform.position = new Vector2( createPosX, posY );
+
+		// Apply physical values to the object's rigidbody.
+		coinRigid.velocity = Vector2.left * speedX; // Multiply the left vector by our speed to obtain velocity.
+	}
+
+}
---
+++ Group4Capstone/Assets/Scripts/CurrencyGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrencyGenerator : MonoBehaviour
+{
+	// Public variables which can be modified in the editor at runtime.
+	public GameObject coin;				 // The coin prefab.
+	public float coinTimer = 10.0f;      // How long we wait before generating another coin.
+
+	void Start()
+	{
+		// Start the infinite coroutine to generate coins.
+		StartCoroutine( GenerateAsteroids() );
+	}
+
+	IEnumerator GenerateAsteroids()
+	{
+		// Continue generating infinitely.
+		while( true )
+		{
+			CreateCoin();
+			yield return new WaitForSeconds( coinTimer );	// Wait a bit to generate another coin.
+		}
+	}
+
+
+
+	public void CreateCoin()
+	{
+		Instantiate( coin );	// Instantiate a new coin.
+	}
+}
