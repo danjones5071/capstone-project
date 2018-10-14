357658bdf6d3951f5312f8b738e6d61c364950a9
Dan Jones <dan@ogovo.com>
2018-10-14 02:57:02 -0400

    Added primitive purchase button to store.

---
+++ Group4Capstone/Assets/Scripts/Store.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Store : MonoBehaviour
+{
+	public enum Weapons {
+		Inferno = 100
+	}
+
+	public void Purchase( string item )
+	{
+		switch( item )
+		{
+			case "Inferno":
+				Debug.Log( "purchase inferno" );
+				References.global.gameMaster.AddToCurrency( -1 * (int)Weapons.Inferno );
+				break;
+		}
+			
+	}
+}
+
+
+public class item
+{
+	public string name;
+	public int price;
+}
